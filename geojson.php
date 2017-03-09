cdad6eea4572b823c76539b7c33986902f525a55
Shelby Cohen <shelbymcohen@gmail.com>
2017-03-09 00:41:08 +0000

    Customized Leaflet and PHP GeoJSON files to pull from my own Google Spreadsheet and get the map working

--- geojson.php
+++ geojson.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/1d-p3Sk73zW9mk_omhv-NLBIiLq3D4UGcPMIXjkH_WcU/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
